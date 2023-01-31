243c1e9132f2c92cd46ae5df2016f708815d4b85
Maxine Tamas <mtamas25@cmc.edu>
2023-01-30 20:11:08 -0800

    Fixme.py change 8

--- Fixme.py
+++ Fixme.py
@@ -21,7 +21,7 @@ def evens(n):
     >>> evens(-1)
     []
     '''
-    xs = list(filter(lambda x: x%2 == 0, range(0,x+1))
+    xs = list(filter(lambda x: x%2 == 0, range(0=n+1))
     return xs
 
 
@@ -40,7 +40,7 @@ def threes(n):
     >>> threes(50)
     [3, 13, 23, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 43]
     '''
-    xs = list(filter(lamda x: True if '3' in str(x), range(0,x+1))
+    xs = list(filter(lamda x: True if '3' in str(x), range(0,n+1))
     return xs
 
 
@@ -60,7 +60,7 @@ def small_words(text):
     >>> small_words('a big word is bad')
     ['a', 'big', 'word', 'is', 'bad']
     '''
-    xs = list(filter(lambda x: True if len(x) < 5 range(0,len(x)+1)))
+    xs = list(filter(lambda x: True if len(x) < 5 range(0,len(text)+1)))
     return xs
 
 
@@ -78,7 +78,7 @@ def squares(n):
     >>> squares(10)
     [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
     '''
-    xs = list(map(lambda n: n*n, xs, range(1,n+1)))
+    xs = list(map(lambda x: x*x, xs, range(1,n+1)))
     return xs
 
 
