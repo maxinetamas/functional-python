80ad6e6830e968dbcc8f62836aa9dfefa4baa343
Maxine Tamas <mtamas25@cmc.edu>
2023-01-30 21:18:50 -0800

    Fixme.py change 14

--- Fixme.py
+++ Fixme.py
@@ -42,7 +42,7 @@ def threes(n):
     [3, 13, 23, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 43]
     '''
     xs = range(0,n+1)
-    xs = list(filter(lambda x: True if '3' in str(x)))
+    xs = list(filter(lambda x: True if '3' in str(x), xs))
     return xs
 
 
@@ -63,7 +63,7 @@ def small_words(text):
     ['a', 'big', 'word', 'is', 'bad']
     '''
     xs = range(0,len(text)+1)
-    xs = list(filter(lambda x: True if len(x) < 5))
+    xs = list(filter(lambda x: True if len(x) < 5, xs))
     return xs
 
 
